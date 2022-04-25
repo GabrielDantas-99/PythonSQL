8e9717f8f9f0f230c9dc2b2650aa75f5e531ce96
Gabriel Dantas <gabrieldantaslp@gmail.com>
2022-04-25 17:00:00 -0300

    Delete from, sql injecction

---
+++ deleteFromBy.py
@@ -0,0 +1,30 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host= "localhost",
+  user= "root",
+  password= "1234567",
+  database = "pythondb"
+)
+
+mycursor = mydb.cursor()
+'''
+# Deletando linhas com o lacation 'Campina'
+# Importante inserir 'WHERE' quando não se quer deletar todas as linhas
+sql = "DELETE FROM users WHERE location = 'Campina'"
+
+mycursor.execute(sql)
+# Necessário para fazer mudanças no bd
+mydb.commit()
+
+print(mycursor.rowcount, 'record(s) deleted')
+'''
+# Previninco SQL Injecction:
+sql = "DELETE FROM users WHERE location = %s"
+loc = ("Caicó",)
+
+mycursor.execute(sql, loc)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record(s) deleted")
